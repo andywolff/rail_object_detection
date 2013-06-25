cb2482861b1cbb152af0655f5ea814737f6f07bd
Andy Wolff <andywolff@wpi.edu>
2013-06-25 10:49:56 -0400

    tweaked the extraction parameters some more

--- rail_object_discovery/launch/extract_objects.sh
+++ rail_object_discovery/launch/extract_objects.sh
@@ -1,7 +1,7 @@
 echo 'calling /discover_objects service with parameters tuned by rail'
 rosservice call /discover_objects "constraints:
-  object_min_sensor_range: 0.4
-  object_max_sensor_range: 3.0
-  object_min_spherical_radius: 0.03
-  object_max_spherical_radius: 0.7
-plane_slope_tolerance: 1.0"
+  object_min_sensor_range: 0.2
+  object_max_sensor_range: 4.0
+  object_min_spherical_radius: 0.01
+  object_max_spherical_radius: 0.1
+plane_slope_tolerance: 2.0"
